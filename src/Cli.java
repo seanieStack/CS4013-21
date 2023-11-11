cf0f3ffe7829b7f2f6dfdb94a54e8080cd4eb9b0
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-11-11 17:06:39 +0000

    Finished Login feature and started QCA calculation feature

--- src/Cli.java
+++ src/Cli.java
@@ -6,9 +6,9 @@ public class Cli {
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in ) ;
     Login login = new Login() ;
+    QcaCalc qcaCalculator = new QcaCalc() ;
 
-
-    // Code to select diviion 
+   
     boolean selectionPassed = false ;
     System.out.println("Choose your division , (S)tudent , (F)aculty , (D)epartment");
     String command = scanner.nextLine().toUpperCase();
@@ -29,16 +29,30 @@ public class Cli {
     if (selectionPassed == true ) {
     System.out.println("Please enter your username (University Number) ");
     String username = scanner.nextLine();
-    login.usernameIsCorrect(username) ;
+    
     System.out.println("Please enter your password ");
     String password = scanner.nextLine();
-    if (login.usernameIsCorrect(username) && login.pwIsCorrect(username,password) == true ) {
+    if (login.correctLogin(username, password,login.getDivison()) == true ) {
         System.out.println("You have succsessfully logged in ") ;
-    } else if ( !login.usernameIsCorrect(username) ) {
-        System.out.println("Please enter a valid username , This username is not in the system ") ; 
     } else if ( username.length() != 8 || password.length() != 6 ) {
         System.out.println("Please enter the correct length Username and password , Username is 8 characthers and password is 6 characthers");
+    } 
+    else if ( login.correctDivison(username, password, login.getDivison())) { // Boolean states may need some work
+        System.out.println( "This username and password belongs to the " + login.getDivison() + " division, not the division you have chosen");
     }
+    else {
+        System.out.println("Please enter the correct username and password (!");
+    } 
 }
+    System.out.println("Enter your command , (C)alculate QCA , (V)iew Transcript,(R)equest repeat" );
+    String command2 = scanner.nextLine().toUpperCase();
+    if( login.getDivison() .equals("Student") && command2.equals("C") ) {
+        System.out.println( " Please enter this semesters grades in the format ,A1 ,A2,B1") ;
+        String gradesInput = scanner.nextLine() ;
+        String[] grades = gradesInput.split(",\\s*"); // This line needs some editing 
+        System.out.println("Cummulative QCA is " + String.format("%.2f",qcaCalculator.getCummulativeQca(grades)) + " for the " +grades.length + " modules you have taken" ); 
+    }
+  
+
  }
 }
