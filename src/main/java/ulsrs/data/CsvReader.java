3d27b74bc7dff09cac2b1e4072d140bdf39793d9
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-11-11 17:22:33 +0000

    Updated CsvReader.java
    Added method to read CSV files and add content to a arraylist

--- src/main/java/ulsrs/data/CsvReader.java
+++ src/main/java/ulsrs/data/CsvReader.java
@@ -1,4 +1,26 @@
-package main.java.ulsrs.data;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CsvReader {
+
+    private String csvFilePath ; 
+    private List<String[]> csvData = new ArrayList<>();
+
+    // Constructor that reads the CSV data
+    public List<String[]> CsvSearch (String csvFilePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                csvData.add(values);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return csvData ; 
+    }
 }
