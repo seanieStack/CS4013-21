e867370207f582d8b6211c6dfd65d2807cbf7259
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-12-03 21:50:52 +0000

    Add JavaDoc Comments

--- src/CommandLineInterface.java
+++ src/CommandLineInterface.java
@@ -1,22 +1,36 @@
+/** Command-line interface class for inteacting with App
+    *Handles user input and directs users to the correct functionality based on their divisions(Student, Faculty, Department)
+*/
 public class CommandLineInterface {
+     //Creates object of the Login class
     Login login;
+
+    /** Runs the command-line interface 
+ * Creates objects of the different classes based off the user division and passes username and password from Login class to them
+*
+*/ 
+    
     public void run() {
-
+        //New login object
         login = new Login();
-
+        //Determines division
         switch (login.getDivision()) {
-            case "Student" -> {
+            case "Student" -> 
+                //Creates Student object and puts the login details into it
                 Student student = new Student(login.getUsername(), login.getPassword());
                 student.studentLogic();
             }
-            case "Faculty" -> {
+            case "Faculty" -> 
+                //Creates Faculty object and puts the login details into it
                 Faculty faculty = new Faculty(login.getUsername(), login.getPassword());
                 faculty.facultyLogic();
             }
             case "Department" -> {
+                //Creates Department object and puts the login details into it
                 Department department = new Department(login.getUsername(), login.getPassword());
                 department.departmentLogic();
             }
+            //Displays Error message
             default -> System.out.println("Invalid Input !");
         }
 
