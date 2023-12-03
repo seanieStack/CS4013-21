91de01ffd989c01ec059d5368a7f26cfa0c0a4cf
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-12-03 21:07:41 +0000

    Add JavaDoc Comments

--- src/App.java
+++ src/App.java
@@ -1,4 +1,13 @@
+/**
+ * This class starts the application.
+ */
+
 public class App {
+       /**
+     * The entry point of the application.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args){
         CommandLineInterface app = new CommandLineInterface();
         
