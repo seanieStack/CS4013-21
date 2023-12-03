ae13585bdcd96ebb4982599f5031fc06524b755f
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-12-03 21:43:02 +0000

    Add JavaDoc Comments

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,17 @@
+/**
+ * Represents a default person with a username and a password.
+ */
 public abstract class Person {
     String username;
     String password;
-
+    
+    /**
+     * Constructs a person with the specified username and password.
+     *
+     * @param username The person's username.
+     * @param password The person's password.
+     */
+    
     public Person(String username, String password){
         this.username = username;
         this.password = password;
