85a2b8e340f703a795f3a7aec15437afaef66a4c
CallanMark <144827341+CallanMark@users.noreply.github.com>
2023-12-03 22:22:26 +0000

    Add JavaDoc Comments

--- src/Student.java
+++ src/Student.java
@@ -1,30 +1,45 @@
 import java.util.Scanner;
 
+    /**Extends the Person class and allows student to view their transcript, their QCA and request actions 
+    */
+
 public class Student extends Person{
     Scanner scanner;
     Transcript transcript;
+
+    /**Constructor for the Student class
+    *@param username -the students username
+    *@param password -the students password
+    */
+    
     public Student(String username, String password){
         super(username, password);
         scanner = new Scanner(System.in);
         transcript = new Transcript();
     }
 
-    public void studentLogic(){
+    /**Method containing all student logic interactions
+    *Allows students to calculate, view transcript or make requests
+    */
 
+    public void studentLogic(){
+        //Gives the options to the studen
         System.out.println("Enter your command (C)alculate QCA (V)iew Transcript (R)equest action" );
         String commandStudent = scanner.nextLine().toUpperCase();
 
         switch (commandStudent) {
             case "C" -> {
-
+                //Handles the QCA calculation
                 System.out.println("Please enter this semesters grades in the format A1,A2,B1");
                 String gradesInput = scanner.nextLine();
-                String[] grades = gradesInput.split(",\\s*"); //FIXME: Needs work
+                String[] grades = gradesInput.split(",\\s*"); 
                 System.out.println("Cummulative QCA is " + String.format("%.2f", QcaCalc.getCummulativeQca(grades)) + " for the " + grades.length + " modules you have taken");
             }
-
+            
+            //Handles the viewing of the transcript
             case "V" -> System.out.println(transcript.printTranscript(username));
 
+            //Handles all student requests
             case "R" -> {
                 System.out.println("Enter your command, (R)equest repeat , (L)ink in module , (Y)ear repeat , (S)emester repeat");
                 String commandRequest = scanner.nextLine().toUpperCase();
@@ -33,8 +48,6 @@ public class Student extends Person{
                         System.out.println("Enter Module code for your requested module");
                         String commandModule = scanner.nextLine().toUpperCase(); //TODO: validate choice - 6 Digit length
 
-                        // TODO: Check module is on transcript , if so add this to student transcript
-                        // Add to student transcript method
                         System.out.println("Your repeat request has being registered and is reflected on your transcript ");
                         transcript.setRequest(" has requested to repeat module: " + commandModule);
                         transcript.recordRequest(username); // Add to CSV file
@@ -42,7 +55,7 @@ public class Student extends Person{
                     }
                     case "L" -> {
                         System.out.println("Enter Module code for your requested Link-in module");
-                        String commandModule = scanner.nextLine().toUpperCase(); //TODO (DONE): validate choice - 6 Digit length, D
+                        String commandModule = scanner.nextLine().toUpperCase(); 
                         if (commandModule.length() != 6) {
                             System.out.println("Module code must have a 6 digit length !") ;
                         }
@@ -50,16 +63,14 @@ public class Student extends Person{
                         transcript.setRequest("has requested to take module : " + commandModule + " as a link in module");
                         transcript.recordRequest(username); // Add to CSV file
 
-                        // Add to student transcript method
+                    
                         System.out.println("Your link in module request has being registered and is reflected on your transcript ");
                     }
                     case "Y" -> {
                         System.out.println("Enter the academic year in which you wish to repeat");
                         String commandYear = scanner.nextLine().toUpperCase();
 
-                        //TODO: validate choice
-                        //Check module is on transcript , if so add this to student transcript
-                        // Add to student transcript method
+                     
 
                         System.out.println("Your repeat request has being registered and is reflected on your transcript ");
                         transcript.recordRequest(username); // Add to CSV file
@@ -69,9 +80,6 @@ public class Student extends Person{
                         System.out.println("Enter the Semester and AY in which you wish to repeat");
                         String commandSemester = scanner.nextLine().toUpperCase();
 
-                        //TODO: validate choice
-                        //Check module is on transcript , if so add this to student transcript
-                        // Add to student transcript method
 
                         transcript.setRequest(" has requested to repeat semester : " + commandSemester);
                         transcript.recordRequest(username); // Add to CSV file
@@ -81,7 +89,7 @@ public class Student extends Person{
             }
         }
 
-        // TODO: Need logic to prevent passing students from requesting repeats
+        
         // COMMENT: We will need Grades.CSV to be fully integrated first , Adding a credits (received or not) field to QcaCalc class
     }
 }
